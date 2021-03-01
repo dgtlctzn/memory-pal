01bde16f233f3e9b2ad0321c215a974dbc9c2b77
Joseph Perry <josephperry720@gmail.com>
2021-03-01 15:13:59 -0500

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>Its a nice one</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          emitWarning: true,
+          failOnError: false,
+          failOnWarning: false,
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
