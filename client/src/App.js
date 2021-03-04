31da3e15cba9940b6a1c146c15cd09bbef85f77b
Joseph Perry <josephperry720@gmail.com>
2021-03-04 13:21:07 -0500

    added react router and signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,16 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import SignUp from "./containers/SignUp/SignUp.jsx";
 
 const App = () => {
-  const [name, setName] = useState("");
-
-  const getSql = async () => {
-    try {
-      const {data} = await axios({
-        method: "GET",
-        url: "https://fd30zc1217.execute-api.us-east-1.amazonaws.com/test/",
-      });
-      console.log(data);
-      setName(data[0].name + " is cool!");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    getSql();
-  }, []);
 
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>{name}</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={SignUp}/>
+      </Switch>
+    </Router>
   );
 };
 
