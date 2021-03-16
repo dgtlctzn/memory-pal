8d77d14b6c067d0770d5671d480edc8fd10c28cd
Joseph Perry <josephperry720@gmail.com>
2021-03-16 13:00:27 -0400

    added test to text events

--- tests/event_text_tests.py
+++ tests/event_text_tests.py
@@ -31,3 +31,23 @@ def test_future_mothers_day():
     res_7_2028 = sql.get_events(seven_days_before_2028, _, mothers_day_2028)
 
     assert len(res_0_2025) == 1 and len(res_7_2025) == 1 and len(res_0_2028) == 1 and len(res_7_2028) == 1
+
+
+def test_future_birthday():
+    test_day_2025 = date(year=2025, month=7, day=20)
+    five_days_before_2025 = date(year=2025, month=7, day=15)
+
+    test_day_2030 = date(year=2030, month=7, day=20)
+    five_days_before_2030 = date(year=2030, month=7, day=15)
+
+    _ = date.today()
+
+    sql = SqlConnect(host, user, password, database)
+
+    res_0_2025 = sql.get_events(test_day_2025, _, _)
+    res_7_2025 = sql.get_events(five_days_before_2025, _, _)
+
+    res_0_2030 = sql.get_events(test_day_2030, _, _)
+    res_7_2030 = sql.get_events(five_days_before_2030, _, _)
+
+    assert len(res_0_2025) == 1 and len(res_7_2025) == 1 and len(res_0_2030) == 1 and len(res_7_2030) == 1
