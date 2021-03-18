c07b819c37c996b6ea5029ff3cb768dc46688624
Joseph Perry <josephperry720@gmail.com>
2021-03-18 13:45:18 -0400

    changed text message for one day till case

--- functions/text_events.py
+++ functions/text_events.py
@@ -53,11 +53,12 @@ class SendText:
         return str(number) + num_str
 
     def text_by_type(self, event_type, name, days_till, phone, message, age):
+        day_str = 'day' if days_till == 1 else 'days'
 
         if event_type == 'Birthday':
             if days_till:
                 text_message = f'''
-                {days_till} more days till it\'s {name}'s birthday! They will be {age} years old. {message}
+                {days_till} more {day_str} till it\'s {name}'s birthday! They will be {age} years old. {message}
                 '''
                 self.text(phone, text_message)
             else:
@@ -66,7 +67,7 @@ class SendText:
         elif event_type == 'Holiday':
             if days_till:
                 text_message = f'''
-                {days_till} more days till {name}! {message}
+                {days_till} more {day_str} till {name}! {message}
                 '''
                 self.text(phone, text_message)
             else:
@@ -75,7 +76,7 @@ class SendText:
         elif event_type == 'Cancel Subscription':
             if days_till:
                 text_message = f'''
-                Remember to cancel your {name} subscription in {days_till} days! {message}
+                Remember to cancel your {name} subscription in {days_till} {day_str}! {message}
                 '''
                 self.text(phone, text_message)
             else:
@@ -84,7 +85,7 @@ class SendText:
         elif event_type == 'Father\'s Day' or event_type == 'Mother\'s Day':
             if days_till:
                 text_message = f'''
-                {event_type} is coming up in {days_till} days! {message}
+                {event_type} is coming up in {days_till} {day_str}! {message}
                 '''
                 self.text(phone, text_message)
             else:
@@ -98,7 +99,7 @@ class SendText:
         else:
             if days_till:
                 text_message = f'''
-                {days_till} days till your {name}. {message}
+                {days_till} {day_str} till your {name}. {message}
                 '''
                 self.text(phone, text_message)
             else:
